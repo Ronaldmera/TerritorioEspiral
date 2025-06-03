2566b408310bc3f0f4d0d6722d41dfaa5df8c0e0
Ronaldmera <ronaldalme1230@gmail.com>
2025-06-03 16:40:26 -0500

    al dar click en opcion de nav, el menu amburguesa se cierra

--- script/hamburguer.js
+++ script/hamburguer.js
@@ -1,14 +1,32 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function () {
   //inicializando AOS
   AOS.init({
     offset: 200, // en píxeles
   });
 
-  const menuBtn = document.getElementById('menu-btn');
+  const menuBtn = document.getElementById("menu-btn");
+  const navbarCollapse = document.getElementById("navbarNavDropdown");
+  const navbarLinks = document.querySelectorAll(".navbar-nav .nav-link");
 
+  // Inicializa el colapso de Bootstrap
+  const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+    toggle: false,
+  });
+
+  // Alternar clase para la animación del botón hamburguesa
   if (menuBtn) {
-    menuBtn.addEventListener('click', function () {
-      menuBtn.classList.toggle('open');
+    menuBtn.addEventListener("click", function () {
+      menuBtn.classList.toggle("open");
     });
   }
+
+  // Cierra el menú y quita animación de botón al hacer clic en un enlace
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      if (navbarCollapse.classList.contains("show")) {
+        bsCollapse.hide();
+        menuBtn.classList.remove("open"); // Cierra la animación del botón
+      }
+    });
+  });
 });
